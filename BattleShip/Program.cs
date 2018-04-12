b5389d73d88bc2c123bc49db03089e77ec4d7391
Zachary Dixon <dixonzac000@alpine.local>
2018-04-12 14:17:15 -0600

    New battleship

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -16,11 +16,11 @@ namespace BattleShip
             
             
 
-            string hit = "Hit";
-            string miss = "miss";
+            //string hit = "Hit";
+            //string miss = "miss";
             
-        //}
-
+       // }
+        //this methods job is to place the ships... problem is that we cant figure out how to use mcculloughs code to our advantage.
         void Placeships() {
             Random rnd = new Random();
             int xax = rnd.Next(0, 11);
@@ -28,56 +28,18 @@ namespace BattleShip
             int yax = rmd.Next(0, 11);
 
             SpeedBoat speedBoat = new SpeedBoat(xax, yax);
-           SeaArray[0, 2] = Cell.LiveBoat;
+           //SeaArray[0, 2] = Cell.LiveBoat;
 
         }
 
         
         
     
-        enum Cell { Water, LiveBoat, HitBoat, SunkBoat };
-
-        static string CellToString(Cell c)
-        {
-            switch (c)
-            {
-                case Cell.Water: return "_";
-                case Cell.LiveBoat: return "O";
-                case Cell.HitBoat: return "*";
-                case Cell.SunkBoat: return "X";
-                default: return "?";
-            }
-        }
-
-        static string BoardToString(Cell[,] board)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            // Column labels
-            sb.Append("  ");
-            for (int i = 1; i <= 10; i++)
-            {
-                sb.Append(i + " ");
-            }
-            sb.AppendLine();
-
-            // Rows
-            for (int i = 0; i < board.GetLength(0); i++)
-            {
-                char rowLabel = (char)('A' + i);
-                sb.Append(rowLabel + " ");
-                for (int j = 0; j < board.GetLength(1); j++)
-                {
-                    Cell c = board[i, j];
-                    sb.Append(CellToString(c) + " ");
-                }
-                sb.AppendLine();
-            }
-            return sb.ToString();
-        }
-         Cell[,] SeaArray = new Cell[10, 10];
+        
         static void Main(string[] args)
         {
+            BattleShip b = new BattleShip();
+            b.Run();
             /*Make the sea array... then make ships.
              * make it so the ships can be placed.
              * then make it so they can be attacked and stored where and what health they have.
@@ -86,19 +48,28 @@ namespace BattleShip
                 */
 
 
-            Placeships();
+
+
+
             
 
-           Cell[,] SeaArray = new Cell[10,10];
-
-            SeaArray[0, 2] = Cell.LiveBoat;
-            SeaArray[2, 2] = Cell.LiveBoat;
-           
-            SeaArray[1, 1] = Cell.HitBoat;
-           
             
-            Console.WriteLine(BoardToString(SeaArray));
-            Console.WriteLine("Enter coordinates for shoots thing");
+           // b.Menu();
+            b.PlayingBoard();
+
+
+            //Console.WriteLine(BoardToString(SeaArray));
+            //Working on the if statements on if there is a ship there where you shoot
+            Console.WriteLine("Enter coordinates for shoots thing x first then y after you hit enter");
+            Console.ReadLine();
+           // int shotx = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+            // int shoty = int.Parse(Console.ReadLine());
+            // if (shot = Cell.LiveBoat) {
+
+
+            // }
+          
 
             Console.ReadKey();
 
